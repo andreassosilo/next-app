0a3677e902abafc85789bff43f7ed51e5ccfb010
andreassosilo <andreas.sosilo@gmail.com>
2024-06-12 20:10:20 +0200

    Add products folder for routing and ProductPage component

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface Props {
+  params: { slug: string[] };
+}
+const ProductPage = ({ params: { slug } }: Props) => {
+  return <div>ProductPage {slug}</div>;
+};
+
+export default ProductPage;
