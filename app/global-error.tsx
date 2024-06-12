ac9355df5c0266eeddb3aa466ffc2edff69c1e63
andreassosilo <andreas.sosilo@gmail.com>
2024-06-12 22:38:20 +0200

    Add error.tsx to handle unexpected errors

---
+++ app/global-error.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+const GlobalErrorPage = () => {
+  return <div>Unexpected error in the RootLayout has occured.</div>;
+};
+
+export default GlobalErrorPage;
