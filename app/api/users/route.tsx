375f78884a52c847269a5441b487a23045c5717a
andreassosilo <andreas.sosilo@gmail.com>
2024-06-17 20:38:27 +0200

    Add POST function to create an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,3 +7,13 @@ export function GET(request: NextRequest) {
     { id: 2, name: 'Doe' },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validate
+  if (!body.name)
+    return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+  // If invalid, return 400
+  // Else, return data
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
